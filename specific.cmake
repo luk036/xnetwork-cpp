72c8a05a7462e587f54210e54ce18ba9d9dc3f58
Wai-Shing Luk <luk036@gmail.com>
2021-07-21 00:11:53 +0800

    support -DCPM_USE_LOCAL_PACKAGES=TRUE

--- specific.cmake
+++ specific.cmake
@@ -1,4 +1,20 @@
 CPMAddPackage(
+  NAME fmt
+  GIT_TAG 7.1.3
+  GITHUB_REPOSITORY fmtlib/fmt
+  OPTIONS "FMT_INSTALL YES" # create an installable target
+)
+
+find_package(Boost REQUIRED)
+if(Boost_FOUND)
+  message(STATUS "Found boost: ${Boost_INCLUDE_DIRS}")
+  # add_library(Boost::boost INTERFACE IMPORTED GLOBAL)
+  target_include_directories(Boost::boost SYSTEM INTERFACE ${Boost_INCLUDE_DIRS})
+  # Disable autolink
+  target_compile_definitions(Boost::boost INTERFACE BOOST_ALL_NO_LIB=1)
+endif()
+
+CPMAddPackage(
   NAME cppcoro
   GIT_TAG 1.0
   GITHUB_REPOSITORY luk036/cppcoro
@@ -20,3 +36,5 @@ elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
   # using Visual Studio C++
   add_compile_options(/std:c++latest /await)
 endif()
+
+set(SPECIFIC_LIBS fmt::fmt Boost::boost cppcoro::cppcoro)
