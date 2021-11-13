c0c4359fcc593ae45722a49178895e7b49bdc8a5
Wai-Shing Luk <luk036@gmail.com>
2021-11-13 04:48:00 +0000

    reformat using --target fix-format

--- specific.cmake
+++ specific.cmake
@@ -1,15 +1,20 @@
-CPMAddPackage(
-  NAME fmt
-  GIT_TAG 7.1.3
-  GITHUB_REPOSITORY fmtlib/fmt
-  OPTIONS "FMT_INSTALL YES" # create an installable target
+cpmaddpackage(
+  NAME
+  fmt
+  GIT_TAG
+  7.1.3
+  GITHUB_REPOSITORY
+  fmtlib/fmt
+  OPTIONS
+  "FMT_INSTALL YES" # create an installable target
 )
 
 find_package(Boost REQUIRED)
 if(Boost_FOUND)
   message(STATUS "Found boost: ${Boost_INCLUDE_DIRS}")
   # add_library(Boost::boost INTERFACE IMPORTED GLOBAL)
-  target_include_directories(Boost::boost SYSTEM INTERFACE ${Boost_INCLUDE_DIRS})
+  target_include_directories(Boost::boost SYSTEM
+                             INTERFACE ${Boost_INCLUDE_DIRS})
   # Disable autolink
   target_compile_definitions(Boost::boost INTERFACE BOOST_ALL_NO_LIB=1)
 endif()
