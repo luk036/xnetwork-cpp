f99c864a6559f1af8531fc8217bd9e1c54ffca32
Wai-Shing Luk <luk036@gmail.com>
2021-11-30 21:08:23 +0800

    put cppcoro in specific.cmake

--- specific.cmake
+++ specific.cmake
@@ -30,4 +30,24 @@ if(Boost_FOUND)
   target_compile_definitions(Boost::boost INTERFACE BOOST_ALL_NO_LIB=1)
 endif()
 
+cpmaddpackage(NAME cppcoro GIT_TAG 1.0 GITHUB_REPOSITORY luk036/cppcoro)
+# print_target_properties(cppcoro)
+if(cppcoro_ADDED)
+  message(STATUS "Found cppcoro: ${cppcoro_SOURCE_DIR}")
+  add_library(cppcoro::cppcoro INTERFACE IMPORTED GLOBAL)
+  target_include_directories(cppcoro::cppcoro SYSTEM
+                             INTERFACE ${cppcoro_SOURCE_DIR}/include)
+endif(cppcoro_ADDED)
+
+if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+  # using GCC
+  add_compile_options(-fcoroutines)
+elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+  # using clang
+  add_compile_options(-fcoroutines-ts -stdlib=libc++)
+elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+  # using Visual Studio C++
+  add_compile_options(/std:c++latest /await)
+endif()
+
 set(SPECIFIC_LIBS Py2Cpp::Py2Cpp fmt::fmt Boost::boost cppcoro::cppcoro)
