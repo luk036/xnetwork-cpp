f99a3d62b6672ebb65fa03925aca958b5a3380ef
Wai-Shing Luk <luk036@gmail.com>
2023-02-15 14:15:32 +0800

    apply fix-format; remove conda from documentation build

--- specific.cmake
+++ specific.cmake
@@ -1,44 +1,38 @@
-cpmaddpackage(
-  NAME
-  fmt
-  GIT_TAG
-  7.1.3
-  GITHUB_REPOSITORY
-  fmtlib/fmt
-  OPTIONS
-  "FMT_INSTALL YES" # create an installable target
+CPMAddPackage(
+  NAME fmt
+  GIT_TAG 7.1.3
+  GITHUB_REPOSITORY fmtlib/fmt
+  OPTIONS "FMT_INSTALL YES" # create an installable target
 )
 
-cpmaddpackage(
-  NAME
-  Py2Cpp
-  GIT_TAG
-  1.3
-  GITHUB_REPOSITORY
-  luk036/py2cpp
-  OPTIONS
-  "INSTALL_ONLY YES" # create an installable target
+CPMAddPackage(
+  NAME Py2Cpp
+  GIT_TAG 1.3
+  GITHUB_REPOSITORY luk036/py2cpp
+  OPTIONS "INSTALL_ONLY YES" # create an installable target
 )
 
 find_package(Boost REQUIRED)
 if(Boost_FOUND)
   message(STATUS "Found boost: ${Boost_INCLUDE_DIRS}")
   # add_library(Boost::boost INTERFACE IMPORTED GLOBAL)
-  target_include_directories(Boost::boost SYSTEM
-                             INTERFACE ${Boost_INCLUDE_DIRS})
+  target_include_directories(Boost::boost SYSTEM INTERFACE ${Boost_INCLUDE_DIRS})
   # Disable autolink
   target_compile_definitions(Boost::boost INTERFACE BOOST_ALL_NO_LIB=1)
 endif()
 
 find_package(Boost REQUIRED)
 
-cpmaddpackage(NAME cppcoro GIT_TAG 2.0 GITHUB_REPOSITORY luk036/cppcoro)
+CPMAddPackage(
+  NAME cppcoro
+  GIT_TAG 2.0
+  GITHUB_REPOSITORY luk036/cppcoro
+)
 # print_target_properties(cppcoro)
 if(cppcoro_ADDED)
   message(STATUS "Found cppcoro: ${cppcoro_SOURCE_DIR}")
   add_library(cppcoro::cppcoro INTERFACE IMPORTED GLOBAL)
-  target_include_directories(cppcoro::cppcoro SYSTEM
-                             INTERFACE ${cppcoro_SOURCE_DIR}/include)
+  target_include_directories(cppcoro::cppcoro SYSTEM INTERFACE ${cppcoro_SOURCE_DIR}/include)
 endif(cppcoro_ADDED)
 
 if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
