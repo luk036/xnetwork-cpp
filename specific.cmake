a369abb055a2351772d3a8d3b49eb2d0dcf582f2
Wai-Shing Luk <luk036@gmail.com>
2023-04-23 16:13:51 +0800

    code cleanup

--- specific.cmake
+++ specific.cmake
@@ -12,14 +12,10 @@ CPMAddPackage(
   OPTIONS "INSTALL_ONLY YES" # create an installable target
 )
 
-# find_package(Boost REQUIRED)
-# if(Boost_FOUND)
-#   message(STATUS "Found boost: ${Boost_INCLUDE_DIRS}")
-#   # add_library(Boost::boost INTERFACE IMPORTED GLOBAL)
-#   target_include_directories(Boost::boost SYSTEM INTERFACE ${Boost_INCLUDE_DIRS})
-#   # Disable autolink
-#   target_compile_definitions(Boost::boost INTERFACE BOOST_ALL_NO_LIB=1)
-# endif()
+# find_package(Boost REQUIRED) if(Boost_FOUND) message(STATUS "Found boost: ${Boost_INCLUDE_DIRS}")
+# # add_library(Boost::boost INTERFACE IMPORTED GLOBAL) target_include_directories(Boost::boost
+# SYSTEM INTERFACE ${Boost_INCLUDE_DIRS}) # Disable autolink target_compile_definitions(Boost::boost
+# INTERFACE BOOST_ALL_NO_LIB=1) endif()
 #
 # find_package(Boost REQUIRED)
 
